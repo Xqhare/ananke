2d11bd7cfc4307042508e1d78ecdbff25153847d
Xqhare <xqhare@web.de>
2023-10-28 17:28:50 +0200

    added file reading and basic constructors

--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,31 @@
-#[derive(Debug, Clone, Copy)]
-struct List {
-    field: Type
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+use crate::task::Task;
+
+#[derive(Debug, Clone)]
+pub struct List {
+    tasks: Vec<Task>,
+}
+
+impl List {
+    pub fn open(filename: &str) -> Self {
+        let file_lines = Self::read_lines(filename);
+        let mut output: Vec<Task> = Vec::new();
+        if let Ok(lines) = file_lines {
+            
+            for line in lines {
+                if let Ok(task) = line {
+                    let made_task: Task = Task::new(task);
+                    output.push(made_task);
+                }
+            }
+        
+        }
+        return List{tasks: output};
+    }
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::env;
+
 mod list;
 mod task;
 
+const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to {NAME} by {AUTHOR}, v. {VERSION}");
+    read_file();
+}
+
+fn read_file() {
+    let path: &str = "./todo-test.txt";
+    list::List::open(path);
+    /* if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            if let Ok(task) = line {
+                println!("l: {task}");
+            }
+        }
+    } */
 }
