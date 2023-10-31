5758bb7d4c4ece28c7381c2c2e11055ae0ff9bc8
Xqhare <xqhare@web.de>
2023-10-31 15:35:14 +0100

    added documentation

--- src/gui.rs
+++ src/gui.rs
@@ -2,25 +2,46 @@ use std::io;
 use std::path::Path;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
-use eframe::egui::{Grid, ScrollArea};
+use eframe::egui::{Grid, ScrollArea, TopBottomPanel};
 use eframe::{run_native, App, egui::{CentralPanel, Ui}, NativeOptions};
 
 use crate::task::Task;
 
+/// The author of the package.
 const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+/// The version of the package.
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+/// The name of the package.
 const NAME: &str = env!("CARGO_PKG_NAME");
 
 // TaskWidget is really the App itself
+
+/// `TaskWidget` contains the App state, and can be thought of like the root of the entire App.
 struct TaskWidget {
+    /// A vector of `Task`, primarily used for itteration. May be removed in the future.
     tasks_vec: Vec<Task>,
+    /// Vector containing the completed state of every task in order.
     completed_vec : Vec<bool>,
+    /// Vector containing the priority state of every task in order, containing a empty String in
+    /// case there isn't a priority state.
     priority_vec: Vec<String>,
+    /// Vector containing the completion date of every task in order, containing a empty String in
+    /// case there isn't a completion date.
     complete_date_vec: Vec<Option<String>>,
+    /// Vector containing the creation date of every task in order, containing a empty String in
+    /// case there isn't a creation date.
     create_date_vec: Vec<Option<String>>,
+    /// Vector containing the main text of every task in order. As this is the minimum of required content of
+    /// a task in the todo.txt format, there will never be an empty string inside.
     task_text: Vec<String>,
+    /// Vector containing the vector of project tags, of every task in order, containing a empty String in
+    /// case there aren't any project tags.
     project_tags_vec: Vec<Option<Vec<String>>>,
+    /// Vector containing the vector of context tags, of every task in order, containing a empty String in
+    /// case there aren't any context tags.
     context_tags_vec: Vec<Option<Vec<String>>>,
+    /// Vector containing the vector of special tags, of every task in order, containing a empty String in
+    /// case there aren't any context tags.
     special_tags_vec: Vec<Option<Vec<String>>>,
 }
 
@@ -51,7 +72,17 @@ struct TaskWidget {
     
 // Soo egui reaaaly doesn't like options... wich isn't a problem now that i know what wreaked havok
 
+
+/// Implementing the Default value for `TaskWidget`, interrogates the task returned from the decoding
+/// steps in `task.rs`.
+///
+/// This is the only way of creating a `TaskWidget`.
 impl Default for TaskWidget {
+    /// This function creates the new `TaskWidget`.
+    ///
+    /// At the moment it has a hard coded location, that is then read by line.
+    /// Each line is then interrogated and the appropriate response saved into the struct fields of
+    /// `TaskWidget`.
     fn default() -> Self {
         let path: &str = "./todo-test.txt";
         let file_lines = Self::read_lines(path);
@@ -129,16 +160,21 @@ impl Default for TaskWidget {
     
 }
 
+/// This implementation of `TaskWidget` really is only for helper, support, breakup functions, or for
+/// gui functions that cannot be implemented in the implementation of `egui::App` for `TaskWidget`.
 impl TaskWidget {
+    /// This helper function reads a file by line from a supplied path (could be an &str of the absolute or relative path for examle).
     fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
         let file = File::open(filename)?;
         Ok(BufReader::new(file).lines())
     }
+    /// This gui function creates the main window with the title, author, version
     fn task_panel(&mut self, ctx: &eframe::egui::Context) {
         CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ScrollArea::vertical().show(ui, |ui| {
-                ui.heading(format!("{NAME} - todo.txt editor"));
+                ui.heading(format!("Ananke - todo.txt editor"));
                 ui.label(format!("by {AUTHOR}, v. {VERSION}"));
+                ui.hyperlink_to(format!("{NAME} on github"), "https://github.com/Xqhare/ananke");
                 let mut counter = 0;
                 let vec_strings = vec!["Completed".to_string(), "Priority".to_string(), "Task".to_string()];
                 let temp = ui.separator();
@@ -178,14 +214,25 @@ impl TaskWidget {
     }
 }
 
+/// This implementaion is the integration with `egui` and can only contain a limited number of
+/// functions.
+///
+/// Most importantly it contains the main loop of the gui.
 impl App for TaskWidget {
     // THIS IS THE MAIN LOOP
+    /// This function is the main loop of ananke, being called as often as possible (60 times/sec I
+    /// think).
+    ///
+    /// It takes over after being indirectly called in `gui.rs::main()`.
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         self.task_panel(ctx);
     }
     
 }
 
+/// The main function should be thought of as the startup function, only defining the `app_name`
+/// and the `NativeOptions` needed for running, and passing them on into `egui::run_native()`.
+/// From here `update()` from `impl App for TaskWidget`
 pub fn main() {
     let app_name = "Ananke";
     let native_options = NativeOptions::default();
