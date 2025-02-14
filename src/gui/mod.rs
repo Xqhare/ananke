f95e6c906f41db046cb30d76d80d2e7a581cd5d6
Xqhare <xqhare@web.de>
2025-02-14 03:36:42 +0100

    add: skeleton startup

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,1 +1,21 @@
-mod state;
+use anansi::List;
+
+use crate::state::{persistent_state::PersistentState, State};
+
+pub struct Ananke {
+    pub state: State,
+    pub entire_list: List,
+}
+
+
+fn get_app_name() -> String {
+    let mut app_name = env!("CARGO_PKG_NAME").to_string();
+    app_name.remove(0);
+    app_name.insert(0, app_name.chars().next().unwrap().to_ascii_uppercase());
+    app_name
+}
+
+pub fn gui_startup() {
+    let app_name = get_app_name();
+    let state = State::new(PersistentState::new());
+}
