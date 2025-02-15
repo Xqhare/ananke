adf6c01d0433475b74b40410b7ea4de8f02075fe
Xqhare <xqhare@web.de>
2025-02-15 23:52:55 +0100

    add: error field to State

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,16 +1,20 @@
 use persistent_state::PersistentState;
 
-mod startup_state;
+use crate::error::AnankeError;
+
+pub mod startup_state;
 pub mod persistent_state;
 
 pub struct State {
     pub persistent_state: PersistentState,
+    pub error: Option<AnankeError>,
 }
 
 impl State {
     pub fn new(persistent_state: PersistentState) -> State {
         State {
             persistent_state,
+            error: None,
         }
     }
 }
