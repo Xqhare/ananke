125ae67d1f5ffbfc247269b3103a4b8c8d3d6eb7
Xqhare <xqhare@web.de>
2025-02-15 23:54:29 +0100

    create: AnankeError

---
+++ src/error/mod.rs
@@ -0,0 +1,24 @@
+
+#[derive(Debug)]
+pub struct AnankeError {
+    pub title: String,
+    pub message: String,
+    pub context: Option<String>,
+}
+
+impl AnankeError {
+    pub fn new<S: Into<String>>(title: S, message: S, context: Option<S>) -> AnankeError {
+        AnankeError {
+            title: title.into(),
+            message: message.into(),
+            context: context.map(|s| s.into()),
+        }
+    }
+}
+
+// Display trait
+impl std::fmt::Display for AnankeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}\n{} - {}", self.title, self.message, self.context.clone().unwrap_or("".to_string()))
+    }
+}
