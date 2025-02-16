9fc47f577a830f49405f4ccd561e76e87a9ef337
Xqhare <xqhare@web.de>
2025-02-16 20:52:05 +0100

    add: menu_bar save button

--- src/gui/menu_bar/mod.rs
+++ src/gui/menu_bar/mod.rs
@@ -9,7 +9,27 @@ impl Ananke {
             if ui.button("About").clicked() {
                 
             };
+            if ui.button("Save").clicked() {
+                self.save_state();
+            };
         });
+    }
+
+    pub fn save(&mut self) {
+        self.save_state();
+        if self.state.error.is_none() {
+            self.save_list();
+        }
+    }
+
+    fn save_list(&mut self) {
+        // TODO
+    }
+
+    pub fn save_state(&mut self) {
+        if let Err(e) = self.state.persistent_state.make_persistent() {
+            self.state.error = Some(e);
+        }
     }
 
 }
