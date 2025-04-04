4736953d2c2c511b810e6e995ccb8bac1f98f3dd
Xqhare <xqhare@web.de>
2025-04-04 17:00:05 +0200

    feat(menu_bar): add Timezone dropdown, load and quit button

--- src/gui/menu_bar/mod.rs
+++ src/gui/menu_bar/mod.rs
@@ -1,4 +1,7 @@
-use eframe::egui::TopBottomPanel;
+use eframe::egui::{ComboBox, TopBottomPanel, Ui};
+use horae::TimeZone;
+
+use crate::error::AnankeError;
 
 use super::Ananke;
 
@@ -6,24 +9,46 @@ impl Ananke {
 
     pub fn menu_bar(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
         TopBottomPanel::top("menu_bar").show(ctx, |ui| {
-            if ui.button("About").clicked() {
-                
-            };
-            if ui.button("Save").clicked() {
-                self.save_state();
-            };
+            ui.horizontal(|ui| {
+                if ui.button("About").clicked() {
+                    self.state.error = Some(AnankeError::new("Test title", "Test message", Some("Test context")));
+                };
+                if ui.button("Load").clicked() {
+                    
+                }
+                if ui.button("Save").clicked() {
+                    self.save();
+                };
+                if ui.button("Save & Quit").clicked() {
+                    self.save();
+                    std::process::exit(0);
+                };
+                ui.separator();
+                ui.add(|ui: &mut Ui| {
+                    ComboBox::from_label("Timezone").selected_text(self.state.persistent_state.timezone.to_string()).show_ui(ui, |ui: &mut Ui| {
+                        for tz in horae::TimeZone::get_all() {
+                            if ui.selectable_value(&mut self.state.persistent_state.timezone, TimeZone::from(tz.clone()), tz.to_string()).clicked() {
+                                self.save_state();
+                            }
+                        }
+                    }).response
+                });
+            });
         });
     }
 
     pub fn save(&mut self) {
         self.save_state();
         if self.state.error.is_none() {
-            self.save_list();
+            match self.save_list() {
+                Ok(()) => (),
+                Err(e) => self.state.error = Some(e),
+            }
         }
     }
 
-    fn save_list(&mut self) {
-        // TODO
+    fn save_list(&mut self) -> Result<(), AnankeError> {
+        todo!()
     }
 
     pub fn save_state(&mut self) {
