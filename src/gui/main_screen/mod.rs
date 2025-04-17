629293592989d24cdce48c15d0fb004e76cba34f
Xqhare <xqhare@web.de>
2025-04-17 20:13:36 +0200

    feat(gui/main_screen): fiddle with ui placement

--- src/gui/main_screen/mod.rs
+++ src/gui/main_screen/mod.rs
@@ -1,27 +1,22 @@
 mod load_file;
 mod editor;
+mod search;
 
-use eframe::egui::{CentralPanel, Grid, ScrollArea};
+use eframe::egui::{CentralPanel, Grid, ScrollArea, Ui};
 
 use super::Ananke;
 
 impl Ananke {
     pub fn main_screen(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            Grid::new("main_screen").num_columns(2).min_col_width(ui.available_width() / 2.5).max_col_width(ui.available_width() / 2.5).show(ui, |ui| {
-                self.editor(ui);
-                self.search(ui);
+            Grid::new("main_screen").num_columns(2).min_col_width(ui.available_width() / 2.7).max_col_width(ui.available_width() / 2.7).spacing((10.0, 10.0)).show(ui, |ui| {
+                    self.editor(ui);
+                    self.search(ui);
             });
+            ui.separator();
             ScrollArea::vertical().show(ui, |ui| {
                 self.task_list(ui);
             });
-        });
-    }
-
-    fn search(&mut self, ui: &mut eframe::egui::Ui) {
-        ui.group(|ui| {
-            
-            ui.label("TODO");
         });
     }
 
