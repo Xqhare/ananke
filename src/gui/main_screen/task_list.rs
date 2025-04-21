0c4bdad69bf45f67d538450c2e1f0a2168f0d293
Xqhare <xqhare@web.de>
2025-04-22 01:34:57 +0200

    feat(gui/main_screen/task_list): continue work on task display

--- src/gui/main_screen/task_list.rs
+++ src/gui/main_screen/task_list.rs
@@ -1,4 +1,9 @@
+use anansi::Task;
 use eframe::egui::Ui;
+use horae::Utc;
+
+
+use crate::error::AnankeError;
 
 use super::Ananke;
 
@@ -7,9 +12,65 @@ impl Ananke {
         for task in self.display_list.tasks() {
             ui.add_sized((ui.available_width(), 0.0), |ui: &mut Ui| {
                 ui.group(|ui| {
+                    ui.vertical_centered_justified(|ui| {
+                        self.is_done(ui, task.clone()); 
+                    });
                     ui.label(format!("{:?}", task));
                 }).response
             });
         }
+    }
+
+    fn is_done(&mut self, ui: &mut Ui, task: Task) {
+        ui.horizontal(|ui| {
+            
+            if task.is_done() {
+                ui.label("Status: Done!");
+                if ui.button("Mark Open").clicked() {
+                    let new_task = task.undone();
+                    match self.entire_list.update(new_task.clone(), task.id()) {
+                        Ok(_) => {
+                            match self.display_list.update(new_task.clone(), task.id()) {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    self.state.error = Some(e.into());
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            self.state.error = Some(e.into());
+                        }
+                    }
+                }
+            } else {
+                ui.label("Status: Open");
+                if ui.button("Mark Completed").clicked() {
+                    let mut now = Utc::now();
+                    now.with_timezone(self.state.persistent_state.timezone);
+                    let new_task = task.done(Some(now.date().to_string().into()));
+                    match new_task {
+                        Ok(new_task) => {
+                            let id = new_task.id();
+                            match self.entire_list.update(new_task.clone(), id) {
+                                Ok(_) => {
+                                    match self.display_list.update(new_task.clone(), id) {
+                                        Ok(_) => {}
+                                        Err(e) => {
+                                            self.state.error = Some(e.into());
+                                        }
+                                    }
+                                }
+                                Err(e) => {
+                                    self.state.error = Some(e.into());
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            self.state.error = Some(e.into());
+                        }
+                    }
+                }
+            }
+        });
     }
 }
